fb0757299666d4b8a6b364b6ae0518f5a1899eaa
kabradshaw1 <itllbok82@gmail.com>
2022-11-17 10:22:50 -0500

    bianary search

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,25 +1,57 @@
 // This adds listeners to all buttons in container
-const containerEl = document.getElementById('container');
+// const containerEl = document.getElementById('container');
 
-const clickHandler = function(event) {
-  if (event.target.matches('button')) {
-    event.target.textContent = 'Clicked!';
-  }
-};
+// const clickHandler = function(event) {
+//   if (event.target.matches('button')) {
+//     event.target.textContent = 'Clicked!';
+//   }
+// };
 
-containerEl.addEventListener('click', clickHandler);
+// containerEl.addEventListener('click', clickHandler);
 // This adds listeners to all buttons
 // const buttons = document.getElementsByTagName('button');
 
 // for (let i = 0; i < buttons.length; i++) {
 //   buttons[i].addEventListener('click', clickHandler());
 // }
+// 17.3.2 algorithm basic example
+// const isEven = (num) => {
+//   if (num % 2 === 0) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// };
+// write an algorithm that console logs the index of a given number in an array
+// const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
 
-const isEven = (num) => {
-  if (num % 2 === 0) {
-    return true;
+// const findIndex = (num) => {
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i] === num) {
+//       console.log(`${num} found at index ${i}`);
+//     }
+//   }
+// };
+
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const binarySearch = (arr, num) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+
+  if (num === arr[middle]) {
+    return 'found it';
   }
-  else {
-    return false;
+  else if (num < arr[middle]) {
+    right = middle;
+    return `bring right down to ${right}`;
+  }
+  else if (num > arr[middle]) {
+    left = middle + 1;
+    return `bring left up to ${left}`;
   }
 };
+
+console.log(binarySearch(data, 38));
