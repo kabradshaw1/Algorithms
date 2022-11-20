1cc90a57f0e6bbb08b9138ac4fbdde847c023e44
kabradshaw1 <itllbok82@gmail.com>
2022-11-20 10:14:07 -0500

    quick sort done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Benchmark = require('benchmark');
-const { bubbleSort } = require('./sort');
+const { bubbleSort, quickSort } = require('./sort');
 
 const numbers = [];
-for (let i = 0; i < 20000; i++) {
+for (let i = 0; i < 10000; i++) {
   numbers.push(Math.floor(Math.random() * 10000) + 1);
 }
 
@@ -11,7 +11,13 @@ const suite = new Benchmark.Suite;
 suite
   .add('bubble sort', function() {
     const testArray = [...numbers];
+
     bubbleSort(testArray);
+  })
+  .add('quick sort', function() {
+    const testArray = [...numbers];
+
+    quickSort(testArray);
   })
   .on('complete', function() {
     this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`));
